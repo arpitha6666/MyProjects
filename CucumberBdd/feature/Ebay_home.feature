eea8c955cf5d18c2b056f5e68127d071c8249b21
arpitha6666 <arpitha.sridhararao@gmail.com>
2021-05-09 12:23:01 +0530

    Adding CuCUmberBDD Sample project

---
+++ CucumberBdd/feature/Ebay_home.feature
@@ -0,0 +1,6 @@
+Feature: Ebay home scenarios
+
+Scenario: Advanced search link
+Given: I am on Ebay home page 
+When: I click on Advanced link 
+Then: I navigate to Advanced search page
---
+++ CucumberBdd/src/test/java/steps/Ebay_HomeStepDefination.java
@@ -0,0 +1,26 @@
+package steps;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class Ebay_HomeStepDefination {
+	
+@Given("I am on Ebay home page")
+public void i_am_on_ebay_home_page() {
+System.out.println("I am on Ebay home page");
+
+}
+
+@When("I click on Advanced link")
+public void i_click_on_advanced_link() {
+	System.out.println("I click on Advanced link");
+    
+}
+
+@Then("I navigate to Advanced search page")
+public void i_navigate_to_advanced_search_page() {
+	System.out.println("I navigate to Advanced search page");
+
+}
+}
